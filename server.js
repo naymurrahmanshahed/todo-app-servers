f1888a7328d5d0ca92088856c42e76b04d97f1ae
naymurrahmanshahed <naymurrahmanshahed@gmail.com>
2023-07-31 23:53:23 +0600

    server run successfully

--- server.js
+++ server.js
@@ -1,1 +1,26 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 
+//express app
+const app = express();
+
+//variables
+const port = process.env.PORT || 5000;
+
+//middlewares
+app.use(express.json());
+
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Welcome to the todo app server" });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on ${port}`);
+});
