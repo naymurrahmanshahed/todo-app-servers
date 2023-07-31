4ef0e4a5a9033eb2006c5fb78c6fb4607dc5a1f2
naymurrahmanshahed <naymurrahmanshahed@gmail.com>
2023-08-01 00:12:14 +0600

    mongodb connected with server

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-
+const mongoose = require("mongoose");
 //express app
 const app = express();
 
 //variables
 const port = process.env.PORT || 5000;
+const mongo_url = process.env.MONGO_URL;
 
 //middlewares
 app.use(express.json());
@@ -21,6 +22,16 @@ app.get("/", (req, res) => {
   res.status(200).json({ message: "Welcome to the todo app server" });
 });
 
-app.listen(port, () => {
-  console.log(`Server running on ${port}`);
-});
+//mongodb
+mongoose
+  .connect(mongo_url, {
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server Connected With mongodb on ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
